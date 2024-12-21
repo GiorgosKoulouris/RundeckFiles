64283f22ebd6de24f958e560e0dda207285a04ef
Giorgos Koulouris <grgkoulouris@yahoo.gr>
2024-12-21 20:38:54 +0200

    sync with tests on dev

--- playbooks/win_install_zabbix_agent/00_win_install_zabbix_agent_rundeck.sh
+++ playbooks/win_install_zabbix_agent/00_win_install_zabbix_agent_rundeck.sh
@@ -17,14 +17,15 @@ passFile=${creds[1]}
 PASSWORD=$(cat $passFile)
 
 /var/lib/rundeck/.local/bin/ansible-playbook "$playbookFile" -i "$ip," -e \
-	"ansible_user=$USERNAME \
-	ansible_password=$PASSWORD \
-	serverIP=$serverIP \
-	statusPort=$statusPort \
-	version=$version \
-	ansible_connection=winrm \
-	ansible_winrm_transport=basic \
-	ansible_winrm_scheme=http \
-	ansible_winrm_server_cert_validation=ignore \
-	ansible_port=5985 \
-	ansible_shell_type=cmd"
+  "ansible_user=$USERNAME \
+  ansible_password=$PASSWORD \
+  serverIP=$serverIP \
+  statusPort=$statusPort \
+  version=$version \
+  ansible_connection=winrm \
+  ansible_winrm_transport=basic \
+  ansible_winrm_scheme=http \
+  ansible_winrm_server_cert_validation=ignore \
+  ansible_port=5985 \
+  ansible_shell_type=cmd"
+
